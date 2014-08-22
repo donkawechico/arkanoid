54b5171b8c52e27df2a624106e5d48e680500e96
Reece Selby <reece.selby@gmail.com>
2014-08-22 01:33:06 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Library/assetDatabase3
    	Library/guidmapper

---
+++ Assets/BrickBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickBehavior : MonoBehaviour {
+	public GameObject Brick;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter() {
+		Destroy (Brick);
+	}
+
+}
