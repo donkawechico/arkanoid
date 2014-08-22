2bed8a5aca96d34ea5298f5e8d9f98d4057841f0
Reece Selby <reece.selby@gmail.com>
2014-08-22 01:30:37 +0100

    Rough camera control

--- Assets/BallBehaviour.cs
+++ Assets/BallBehaviour.cs
@@ -9,7 +9,7 @@ public class BallBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//ball.rigidbody.AddForce(direction * 100 * speed);
+
 	}
 	
 	// Update is called once per frame
@@ -18,17 +18,9 @@ public class BallBehaviour : MonoBehaviour {
 		//Debug.Log(ball.rigidbody.velocity);
 	}
 
-	// Legacy methods - ideally the physics engine will handle reflections
 	void OnCollisionEnter (Collision collision) {
 		Debug.Log("Collision");
 		ContactPoint[] contact = collision.contacts;
 		direction = Vector3.Reflect(direction, contact[0].normal);
-	}
-
-	Vector3 Bounce (Vector3 reflection) {
-		float orix = reflection.x;
-		float oriy = reflection.y;
-		float oriz = reflection.z;
-		return new Vector3(-orix, -oriy, -oriz);
 	}
 }
---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 ballPosition = ball.transform.position;
+		Camera.main.transform.eulerAngles = new Vector3(Mathf.Clamp(-ballPosition.x, -16, 16), 270, 0);
+	}
+}
